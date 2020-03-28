c93987aa62d05408f62fe63ecfa2b583f2f66efd
Alexander Sidorov <alex.n.sidorov@gmail.com>
2020-03-28 21:33:56 +0300

    added themes

--- src/project/urls.py
+++ src/project/urls.py
@@ -1,15 +1,21 @@
+from datetime import datetime
 from pathlib import Path
 
+import pytz
+import requests
 from django.conf import settings
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path
+from ipware import get_client_ip
 
 HTML_INDEX: Path = settings.REPO_DIR / "index.html"
 HTML_PROJECTS: Path = settings.REPO_DIR / "projects.html"
 HTML_RESUME: Path = settings.REPO_DIR / "resume.html"
 HTML_THOUGHTS: Path = settings.REPO_DIR / "thoughts.html"
 JPG_ME: Path = settings.REPO_DIR / "me.jpg"
+CSS_LIGHT: Path = settings.REPO_DIR / "style_light.css"
+CSS_DARK: Path = settings.REPO_DIR / "style_dark.css"
 
 
 def view_index(*_args, **__kwargs):
@@ -37,11 +43,33 @@ def view_me_jpg(*_args, **__kwargs):
         return HttpResponse(src.read(), content_type="image/jpeg")
 
 
+def view_css(request, *_args, **__kwargs):
+    hour = get_user_hour(request)
+    css = CSS_LIGHT if (9 <= hour <= 17) else CSS_DARK
+    with css.open() as src:
+        return HttpResponse(src.read(), content_type="text/css")
+
+
+def get_user_hour(request):
+    ip = get_client_ip(request)[0]
+    resp = requests.get(f"http://ip-api.com/json/{ip}")
+    payload = resp.json()
+    at_this_moment = datetime.now()
+    if "timezone" not in payload:
+        hour = at_this_moment.hour
+    else:
+        tz_name = payload["timezone"]
+        tz = pytz.timezone(tz_name)
+        hour = pytz.utc.localize(datetime.now()).astimezone(tz).hour
+    return hour
+
+
 urlpatterns = [
-    path("admin/", admin.site.urls),
     path("", view_index),
-    path("resume/", view_resume),
-    path("projects/", view_projects),
-    path("thoughts/", view_thoughts),
+    path("admin/", admin.site.urls),
+    path("css/", view_css),
     path("me/", view_me_jpg),
+    path("projects/", view_projects),
+    path("resume/", view_resume),
+    path("thoughts/", view_thoughts),
 ]
