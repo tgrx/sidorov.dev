f1b7ec00e1d58b25c7c36beb87999e28665e5fc3
Alexander Sidorov <alex.n.sidorov@gmail.com>
2020-04-07 01:46:32 +0300

    introduced utils.xdatetime

--- src/project/urls.py
+++ src/project/urls.py
@@ -1,8 +1,5 @@
-from datetime import datetime
 from pathlib import Path
 
-import pytz
-import requests
 from django.conf import settings
 from django.contrib import admin
 from django.http import Http404
@@ -13,8 +10,8 @@ from django.urls import include
 from django.urls import path
 from django.views.decorators.cache import cache_control
 from django.views.decorators.cache import never_cache
-from ipware import get_client_ip
 from project.utils import consts
+from project.utils.xdatetime import get_user_hour
 
 STATIC_DIR = settings.PROJECT_DIR / "static"
 
@@ -44,22 +41,6 @@ def render_static(file_path: Path, content_type: str) -> HttpResponse:
 
     response = HttpResponse(content, content_type=content_type)
     return response
-
-
-def get_user_hour(request: HttpRequest) -> int:
-    ip = get_client_ip(request)[0]
-    resp = requests.get(f"http://ip-api.com/json/{ip}")
-    payload = resp.json()
-    at_this_moment = datetime.now()
-
-    if "timezone" not in payload:
-        hour = at_this_moment.hour
-    else:
-        tz_name = payload["timezone"]
-        tz = pytz.timezone(tz_name)
-        hour = pytz.utc.localize(datetime.now()).astimezone(tz).hour
-
-    return hour
 
 
 def get_theme_css(hour: int) -> Path:
