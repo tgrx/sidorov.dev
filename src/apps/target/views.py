ba95c2d7ac8cd68a6976ff27f538064665f0c3ef
Alexander Sidorov <alex.n.sidorov@gmail.com>
2020-04-10 23:32:35 +0300

    used TemplateView for target app

--- src/apps/target/views.py
+++ src/apps/target/views.py
@@ -1,11 +1,5 @@
-from django.http import HttpRequest
-from django.http import HttpResponse
-from django.shortcuts import render
-from django.views.decorators.cache import cache_control
-
-from project.utils import consts
+from django.views.generic import TemplateView
 
 
-@cache_control(max_age=consts.AGE_1DAY)
-def view_index(request: HttpRequest) -> HttpResponse:
-    return render(request, "target/index.html")
+class IndexView(TemplateView):
+    template_name = "target/index.html"
