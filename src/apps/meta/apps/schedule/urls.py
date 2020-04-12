3a5b0d64d48d7b14bb113bfa23662d587b67c407
Alexander Sidorov <alex.n.sidorov@gmail.com>
2020-04-12 05:27:34 +0300

    added url & view stub for ocb

--- src/apps/meta/apps/schedule/urls.py
+++ src/apps/meta/apps/schedule/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
+from apps.meta.apps.schedule import views
 from apps.meta.apps.schedule.apps import ScheduleConfig
-from apps.meta.apps.schedule.views import IndexView
 
 app_name = ScheduleConfig.label
 
 urlpatterns = [
-    path("", IndexView.as_view(), name="index"),
+    path("", views.IndexView.as_view(), name="index"),
+    path("ocb/", views.OAuth2CallbackView.as_view()),
 ]
--- src/apps/meta/apps/schedule/views.py
+++ src/apps/meta/apps/schedule/views.py
@@ -1,4 +1,5 @@
 from django.utils.decorators import method_decorator
+from django.views import View
 from django.views.decorators.cache import never_cache
 from django.views.generic import TemplateView
 
@@ -6,3 +7,7 @@ from django.views.generic import TemplateView
 @method_decorator(never_cache, name="get")
 class IndexView(TemplateView):
     template_name = "schedule/index.html"
+
+
+class OAuth2CallbackView(View):
+    pass
