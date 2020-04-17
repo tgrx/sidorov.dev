ce9bdf4dbef8d7c3133947ea8b9412ac4ea7ab30
Alexander Sidorov <alex.n.sidorov@gmail.com>
2020-04-17 22:00:09 +0300

    1

--- src/apps/target/urls.py
+++ src/apps/target/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from django.views.generic import TemplateView
 
 from apps.target.apps import TargetConfig
+from apps.target.views import IndexView
 
 app_name = TargetConfig.label
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name="target/index.html"), name="index"),
+    path("", IndexView.as_view(), name="index"),
 ]
