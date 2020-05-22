7da7f6d8882f13f42dac9f4626c2c74033360139
Alexander Sidorov <alex.n.sidorov@gmail.com>
2020-05-22 15:52:46 +0300

    Photos (#35)
    * implemented image upload
    
    * formatted
    
    * added missing migration

--- src/applications/meta/applications/blog/models.py
+++ src/applications/meta/applications/blog/models.py
@@ -1,6 +1,9 @@
+import uuid
+
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse_lazy
+from storages.backends.s3boto3 import S3Boto3Storage
 
 User = get_user_model()
 
@@ -50,3 +53,9 @@ class Comment(models.Model):
 
         self.nr_dislikes += 1
         self.save()
+
+
+class Photo(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="photos")
+    original = models.FileField(storage=S3Boto3Storage())
