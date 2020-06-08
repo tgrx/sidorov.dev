d99c41ca0fb61549d14d9c55ea9b902f5803ccba
Alexander Sidorov <alex.n.sidorov@gmail.com>
2020-06-08 21:01:45 +0300

    xxx

--- serverless/src/resizer/lambda_function.py
+++ serverless/src/resizer/lambda_function.py
@@ -1,10 +1,18 @@
+import os
 from io import BytesIO
 from typing import NamedTuple
 
 import boto3
+import requests
 from PIL import Image
 
 S3 = boto3.client("s3")
+
+API_URL = os.getenv("API_URL")
+assert API_URL
+
+TOKEN = os.getenv("TOKEN")
+assert TOKEN
 
 
 class S3File(NamedTuple):
@@ -18,6 +26,7 @@ def lambda_handler(event, context):
     files = get_files(event)
     for s3_file in files:
         resize(s3_file)
+        patch(s3_file)
 
 
 def get_files(event: dict):
@@ -26,7 +35,7 @@ def get_files(event: dict):
         bucket = s3["bucket"]["name"]
         key = s3["object"]["key"]
 
-        print(f"processing: {bucket}//{key}")
+        print(f"processing: {bucket} | {key}")
 
         if "thumbnail" in key:
             print("skip thumbnail")
@@ -62,7 +71,7 @@ def resize(s3_file: S3File):
             "ContentType": f"image/{image_format.lower()}",
         },
     )
-    print(f"uploaded to: {s3_file.bucket}//{new_key}")
+    print(f"uploaded to: {s3_file.bucket} | {new_key}")
 
 
 def download_image(s3_file: S3File):
@@ -71,3 +80,21 @@ def download_image(s3_file: S3File):
     print(f"downloaded: {s3_file}: {image.tell()} bytes")
     image.seek(0)
     return image
+
+
+def patch(s3_file: S3File):
+    location, image_file = s3_file.key.split("/")
+    image_file_parts = image_file.split(".")
+    image_file_name = ".".join(image_file_parts[:-1])
+    image_file_ext = image_file_parts[-1]
+    thumb_name = f"{image_file_name}__thumbnail.{image_file_ext}"
+
+    resp = requests.get(f"{API_URL}/", headers={"AUTHORIZATION": f"Token {TOKEN}"})
+    payload = resp.json()
+    obj = next(filter(lambda elm: image_file_name in elm["original"], payload))
+    resp = requests.patch(
+        f"{API_URL}/{obj['uuid']}/",
+        json={"thumbnail": thumb_name},
+        headers={"AUTHORIZATION": f"Token {TOKEN}"},
+    )
+    print(f"XXX patch resp: {resp}")
